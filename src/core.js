0fe0499e97c7bb0a2895b90064d786f3589ba122
mar-tina <martinakhaemba@gmail.com>
2020-02-18 18:49:19 +0300

    initial setup

---
+++ src/core.js
@@ -0,0 +1,52 @@
+export let Shadow;
+
+(function(Shadow) {
+  (function(Base) {
+    let BaseElement = (function() {
+      function BaseElement() {}
+      BaseElement.prototype.clone = function(state) {
+        this.email = "tina@gmail.com";
+        this.noemala = "tina@gmail.com";
+        let noname = "Martina";
+
+        let clone = class extends HTMLElement {
+          constructor() {
+            super();
+            this.state = state;
+
+            const renderTemplate = document.createElement("template");
+            this._shadowRoot = this.attachShadow({
+              mode: "open"
+            });
+
+            let newTemplate = html`
+              <div>Something is here ${this.state.name}</div>
+            `;
+
+            renderTemplate.innerHTML = newTemplate.doc;
+            this._shadowRoot.appendChild(
+              renderTemplate.content.cloneNode(true)
+            );
+
+            return this;
+          }
+        };
+        for (var attr in this) {
+          console.log("the attr", attr);
+          clone[attr] = this[attr];
+        }
+        return clone;
+      };
+      return BaseElement;
+    })();
+    Base.BaseElement = BaseElement;
+  })(Shadow.Base || (Shadow.Base = {}));
+})(Shadow || (Shadow = {}));
+
+console.log("The unl", Shadow.Base.BaseElement.prototype.clone);
+
+export let ShadowElement = args => {
+  let newClass = new Unf.Base.BaseElement.prototype.clone(args);
+
+  customElements.define("class-one", newClass);
+};
