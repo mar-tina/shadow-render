59808c3f28734065b0d1686bb50504f8ed30311c
mar-tina <martinakhaemba@gmail.com>
2020-02-18 21:07:14 +0300

    Bind initial element to document

---
+++ examples/itunes-app/src/index.js
@@ -0,0 +1,16 @@
+import { init, html } from "../../../src/core.js";
+
+import "./components/app.js";
+
+let newtemplate = html`
+  <div>
+    <p>Welcome to the page</p>
+    <div>
+      <main-app> </main-app>
+    </div>
+  </div>
+`;
+
+const myTemplate = () => newtemplate;
+
+init("#app", myTemplate());
