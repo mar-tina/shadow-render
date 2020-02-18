9780ef70111e4e7e2a0bed0f3afbe50d8e35e0e4
mar-tina <martinakhaemba@gmail.com>
2020-02-18 22:20:14 +0300

    setup methods and lifecycle events

--- examples/itunes-app/src/components/app.js
+++ examples/itunes-app/src/components/app.js
@@ -5,9 +5,22 @@ let Myapp = createShadowElement({
     name: "Welcome to the shadows"
   },
 
-  template: state => {
+  methods: {
+    testMethod: () => {
+      console.log("Inside test method", this);
+    }
+  },
+
+  lifecycle: {
+    onMount: ctx => {
+      console.log("mounted", ctx);
+    }
+  },
+
+  template: (state, methods) => {
     return html`
       <div>Inside main app ${state.name}</div>
+      <div>${console.log("The available methods", methods)}</div>
     `;
   }
 });
--- src/core.js
+++ src/core.js
@@ -5,45 +5,6 @@ export const html = (strings, ...args) => ({
   )
 });
 
-export let Shadow;
-
-(function(Shadow) {
-  (function(Base) {
-    let BaseElement = (function() {
-      function BaseElement() {}
-      BaseElement.prototype.clone = function(args) {
-        let clone = class extends HTMLElement {
-          constructor() {
-            super();
-            this.state = args.state;
-
-            const renderTemplate = document.createElement("template");
-            this._shadowRoot = this.attachShadow({
-              mode: "open"
-            });
-
-            renderTemplate.innerHTML = args.template(this.state).doc;
-            this._shadowRoot.appendChild(
-              renderTemplate.content.cloneNode(true)
-            );
-
-            return this;
-          }
-        };
-
-        // for (var attr in this) {
-        //   console.log("the attr", attr);
-        //   clone[attr] = this[attr];
-        // }
-
-        return clone;
-      };
-      return BaseElement;
-    })();
-    Base.BaseElement = BaseElement;
-  })(Shadow.Base || (Shadow.Base = {}));
-})(Shadow || (Shadow = {}));
-
 /**
  * Initializes the application .
  * @param {string} selector
@@ -59,10 +20,67 @@ export let init = function(selector, component) {
   el.shadowRoot.innerHTML = component.doc;
 };
 
+/**
+ * Passes the 'this' object to all the executing functions when node is mounted or unmounted
+ * @param {THIS} elem - object that has reference to the current execution context
+ * @param {function} f - onmount function to be called in the connectedCallback function
+ */
+let bindCycle = (elem, f) => {
+  if (typeof f !== "undefined") {
+    f(elem);
+  } else {
+    console.log("Onmount function is undefined");
+  }
+};
+
+export let Shadow;
+
+(function(Shadow) {
+  (function(Base) {
+    let BaseElement = (function() {
+      function BaseElement() {}
+      BaseElement.prototype.clone = function(args) {
+        let clone = class extends HTMLElement {
+          connectedCallback() {
+            bindCycle(this, args.lifecycle.onMount);
+          }
+          constructor() {
+            super();
+            this.state = args.state;
+            this.methods = args.methods;
+
+            const renderTemplate = document.createElement("template");
+            this._shadowRoot = this.attachShadow({
+              mode: "open"
+            });
+
+            renderTemplate.innerHTML = args.template(
+              this.state,
+              this.methods
+            ).doc;
+            this._shadowRoot.appendChild(
+              renderTemplate.content.cloneNode(true)
+            );
+
+            return this;
+          }
+        };
+
+        // for (var attr in this) {
+        //   clone[attr] = this[attr];
+        //   console.log("the attr", attr);
+        // }
+
+        return clone;
+      };
+      return BaseElement;
+    })();
+    Base.BaseElement = BaseElement;
+  })(Shadow.Base || (Shadow.Base = {}));
+})(Shadow || (Shadow = {}));
+
 export let createShadowElement = args => {
   let newClass = new Shadow.Base.BaseElement.prototype.clone(args);
 
   return newClass;
 };
-
-
