4fc6c0c9410f7bae3d613e3562b8e841986de146
mar-tina <martinakhaemba@gmail.com>
2020-02-20 12:43:37 +0300

    render the main-app element

--- examples/itunes-app/src/index.js
+++ examples/itunes-app/src/index.js
@@ -2,14 +2,14 @@ import { init, html } from "../../../src/core.js";
 
 import "./components/app.js";
 
-let newtemplate = html`
-  <div>
+let newtemplate = html(
+  `<div>
     <p>Welcome to the page</p>
     <div>
       <main-app> </main-app>
     </div>
-  </div>
-`;
+  </div>`
+);
 
 const myTemplate = () => newtemplate;
 
