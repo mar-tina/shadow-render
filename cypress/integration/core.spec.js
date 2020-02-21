a24112de720ced0b5c86c6e59a2b5198f3c45a8e
mar-tina <martinakhaemba@gmail.com>
2020-02-21 17:20:37 +0300

    added initial test

--- cypress/integration/core.spec.js
+++ cypress/integration/core.spec.js
@@ -1,9 +1,25 @@
-import { html, init } from "../../dist/index.js";
+import { html, init } from "../../src/core.js";
 
-describe("My First Test", function() {
-  let doc = document.implementation.createHTMLDocument("New Document");
+let doc = document.implementation.createHTMLDocument("New Document");
+let ndiv = doc.createElement("div");
+ndiv.innerHTML = ` Hello world `;
+ndiv.id = "app";
 
-  it("Asserts that document exists", function() {
-    expect(doc).to.exist;
+doc.body.appendChild(ndiv);
+
+describe("Rendering Tests", function() {
+  it("Asserts that init works", function() {
+    let newtemplate = html(
+      `<div> 
+            <p>Welcome to the page</p> 
+        </div>`
+    );
+
+    const myTemplate = () => newtemplate;
+    init("#app", myTemplate(), doc);
+
+    expect(
+      doc.body.childNodes[0].shadowRoot.childNodes[0].innerHTML.trim()
+    ).to.eq(`<p>Welcome to the page</p>`);
   });
 });
