f7d66f339b2b6b9ada849459bc04b60c39b2d6c6
mar-tina <martinakhaemba@gmail.com>
2020-02-23 15:35:13 +0300

    file cleanup:

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,3 @@
-// const path = require("path");
-
-// module.exports = {
-//   entry: path.resolve(__dirname, "src/core.js"),
-//   output: {
-//     path: path.resolve(__dirname, "dist"),
-//     filename: "index.js",
-//     library: "$",
-//     libraryTarget: "umd"
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js)$/,
-//         exclude: /node_modules/,
-//         use: ["babel-loader"],
-//         query: {
-//           presets: ["es2016", "stage-2"],
-//           plugins: ["@babel/plugin-proposal-decorators", { legacy: true }]
-//         }
-//       }
-//     ]
-//   },
-//   resolve: {
-//     extensions: [".js"],
-//     modules: [path.resolve(__dirname, "src")]
-//   },
-//   mode: "development",
-//   devtool: "sourceMap"
-// };
-
 const path = require("path");
 
 module.exports = {
