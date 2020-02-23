f062b5a96217e28b4894bbceaa52f1c2ce1ba48e
mar-tina <martinakhaemba@gmail.com>
2020-02-23 22:31:57 +0300

    modified config

--- examples/todo-app/webpack.config.js
+++ examples/todo-app/webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
             presets: [["@babel/preset-env"], "@babel/preset-react"],
             plugins: [
               "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-transform-async-to-generator"
             ]
           }
         }
