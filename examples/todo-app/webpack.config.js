2222876b52e5f857cfc1eb9ea922270bcd1bb09c
mar-tina <martinakhaemba@gmail.com>
2020-02-25 19:31:08 +0300

    issue with gh pages

--- examples/todo-app/webpack.config.js
+++ examples/todo-app/webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require("webpack");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "../../sample"),
+    path: path.resolve(__dirname, "dist"),
     filename: "index.js"
   },
   mode: "production",
