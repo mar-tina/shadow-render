cb0465dbbe60d8b1cd44bff7c78b4d7f1d83d21a
mar-tina <martinakhaemba@gmail.com>
2020-02-27 12:38:14 +0300

    setup route component

--- examples/todo-app/src/components/app.js
+++ examples/todo-app/src/components/app.js
@@ -1,13 +1,27 @@
 import { html, createShadowElement } from "../../../../index.js";
-import { mainContainer } from "./appStyle.js";
+import { mainContainer, routeComponent } from "./appStyle.js";
 import "./todolist.js";
 import "./todo.js";
-// import "./counter.js";
+import "./router";
 
 let Myapp = createShadowElement({
+  state: {
+    currentpath: ""
+  },
+
   lifecycle: {
     onMount: () => {
       console.log("Mounted App");
+    }
+  },
+
+  methods: {
+    linkme: (e, args) => {
+      window.location.hash = args.bound.substr(1);
+      //We just need to trigger a re-render
+      args.ctx.setState({
+        currentpath: window.location.hash
+      });
     }
   },
 
@@ -15,9 +29,17 @@ let Myapp = createShadowElement({
     return html(`
         <div style="${mainContainer}">
           <p> TODOS </p>
-          <todo-el> </todo-el>
-          <todo-list> </todo-list>
-        </div>`);
+          <a @onclick="linkme" bind="/#home" id="home-link"> Home </a>
+          <a @onclick="linkme" bind="/#away" id="away-link"> Todos </a>
+        </div>
+        <div style="${routeComponent}">
+          <away-route type="router" path="/#away" > </away-route>
+        </div>
+        <div style="${routeComponent}">
+          <home-route type="router" path="/#home" > </home-route>
+        </div>
+       
+        `);
   }
 });
 
