ed5f4935b51641a9d7981f6cf4a5b5c175518dec
mar-tina <martinakhaemba@gmail.com>
2020-03-22 22:44:40 +0300

    added usestate functionality

--- index.js
+++ index.js
@@ -2,54 +2,69 @@ const parseRange = document.createRange();
 const parse = Range.prototype.createContextualFragment.bind(parseRange);
 
 export let initApp = (id, content, doc) => {
+  let entrydiv = document.createElement("div");
+  entrydiv.innerHTML = content();
   let docref = doc.body.querySelector(id);
-  docref.appendChild(content);
+  docref.appendChild(entrydiv);
 };
 
 export function Shadow(label, args) {
-  let parsedTemplate = parse(args.template);
   let VDomNodes = {};
-
-  console.log("All the args", args);
-
-  let template = document.createElement("div");
-  template.appendChild(parsedTemplate);
 
   customElements.define(
     label,
     class extends HTMLElement {
       connectedCallback() {
         !!args.onMount ? args.onMount() : {};
-        this._shadowRoot.appendChild(template.cloneNode(true));
-        recursivelyCheckForNodes(this._shadowRoot, VDomNodes);
-        this.setAttributes();
+
+        this.render();
       }
       constructor() {
         super();
         this._shadowRoot = this.attachShadow({
           mode: "open"
         });
-        console.log("Running in constructor");
+        this.parsedTemplate = parse(args.template(this));
+        this.template = document.createElement("div");
+        this.template.appendChild(this.parsedTemplate);
+        !!args.getInitialProps
+          ? ([this.state, this.setState, this.bindState] = args.getInitialProps(
+              this
+            ))
+          : {};
+      }
+
+      render() {
+        this._shadowRoot.appendChild(this.template.cloneNode(true));
+        recursivelyCheckForNodes(this._shadowRoot, VDomNodes);
+        this.setAttributes();
+      }
+
+      clear() {
+        for (var i = 0; i < this._shadowRoot.childNodes.length; i++) {
+          this._shadowRoot.removeChild(this.shadowRoot.childNodes[i]);
+        }
       }
 
       setAttributes() {
         for (var key in VDomNodes) {
           if (VDomNodes.hasOwnProperty(key)) {
-            if (!!VDomNodes[key].parent) {
+            if (VDomNodes[key].parent != undefined) {
               // Get element in template and modify element
               let el = this._shadowRoot.querySelector(`#${key}`);
               let attr;
               !!el ? (attr = el.attributes) : (attr = {});
-
               for (var prop in attr) {
-                if (!!attr[prop].nodeName) {
+                if (attr[prop].nodeName != null) {
                   if (attr[prop].nodeName.startsWith("@")) {
-                    attr[prop].nodeName === "bind";
+                    attr[prop].nodeName === "@bind"
+                      ? this.bindState(el, attr[prop].nodeValue)
+                      : {};
                     let f = args.methods[`${attr[prop].nodeValue}`];
                     el.addEventListener(`${attr[prop].nodeName.substr(1)}`, e =>
-                      f(e)
+                      f(e, this)
                     );
-                    el.removeAttribute(`${attr[prop].nodeName}`);
+                    // el.removeAttribute(`${attr[prop].nodeName}`);
                   }
                 }
               }
@@ -60,7 +75,7 @@ export function Shadow(label, args) {
     }
   );
 
-  return template;
+  return args.template;
 }
 
 function recursivelyCheckForNodes(node, allNodes) {
@@ -82,8 +97,8 @@ function recursivelyCheckForNodes(node, allNodes) {
   }
 }
 
-/*** HOOKS Implementation */
-export function useState(state) {
+/*** HOOK-ALIKE Implementation */
+export function useState(state, ctx) {
   const handler = {
     get(target, property, receiver) {
       return Reflect.get(target, property, receiver);
@@ -93,24 +108,35 @@ export function useState(state) {
       return Reflect.set(target, property, value);
     }
   };
-
   let proxyObject = new Proxy(state, handler);
 
-  let setState = createStateHandler(proxyObject);
-  return [state, setState];
+  let bindState = (target, value) => {
+    console.log("The target", target);
+    proxyObject.state = state;
+    target.innerText = proxyObject.state[`${value}`];
+  };
+
+  let setState = createStateHandler(proxyObject, ctx);
+  return [state, setState, bindState];
 }
 
-let createStateHandler = proxy => state => {
-  for (var key in proxyObject) {
+let createStateHandler = (proxy, self) => state => {
+  for (var key in proxy) {
     if (state.hasOwnProperty(key)) {
       proxy[key] = state[key];
+      self.clear();
+      self.render();
     }
   }
 };
 
-// Implementation of the Javascript proxy object
+function randStr() {
+  return Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, "")
+    .substr(0, 5);
+}
 
-/**** HELPERS  *****/
 // A package by: lukeed Github Repository link: https://github.com/lukeed/uid/blob/master/src/index.js
 var IDX = 36,
   HEX = "";
@@ -121,11 +147,4 @@ function uuid(len) {
     num = len || 11;
   while (num--) str += HEX[(Math.random() * 36) | 0];
   return str;
-}
-
-function randStr() {
-  return Math.random()
-    .toString(36)
-    .replace(/[^a-z]+/g, "")
-    .substr(0, 5);
 }
