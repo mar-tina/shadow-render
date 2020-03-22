fbcc9eaa76b7e5ab3973fa1e1eadf939c127bfb0
mar-tina <martinakhaemba@gmail.com>
2020-03-22 15:59:46 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,98 @@
 const parseRange = document.createRange();
 const parse = Range.prototype.createContextualFragment.bind(parseRange);
+
+export function Ombra(label, args) {
+  let parsedTemplate = parse(args.template);
+  let VDomNodes = {};
+
+  let template = document.createElement("div");
+  template.appendChild(parsedTemplate);
+
+  customElements.define(
+    label,
+    class extends HTMLElement {
+      constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({
+          mode: "open"
+        });
+
+        this._shadowRoot.appendChild(template.cloneNode(true));
+        recursivelyCheckForNodes(this._shadowRoot, VDomNodes);
+        this.setAttributes();
+      }
+
+      setAttributes() {
+        for (var key in VDomNodes) {
+          if (VDomNodes.hasOwnProperty(key)) {
+            if (!!VDomNodes[key].parent) {
+              // Get element in template and modify element
+              let el = this._shadowRoot.querySelector(`#${key}`);
+              let attr;
+              !!el ? (attr = el.attributes) : (attr = {});
+
+              for (var key in attr) {
+                if (!!attr[key].nodeName) {
+                  if (attr[key].nodeName.startsWith("@")) {
+                    let f = args.methods[`${attr[key].nodeValue}`];
+                    el.addEventListener(`${attr[key].nodeName.substr(1)}`, e =>
+                      f(e)
+                    );
+                    el.removeAttribute(`${attr[key].nodeName}`);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  );
+
+  return template;
+}
+
+export let initApp = (id, content, doc) => {
+  let docref = doc.body.querySelector(id);
+  docref.appendChild(content);
+};
+
+function recursivelyCheckForNodes(node, allNodes) {
+  let DomNode = {};
+  node.id == "" ? (node.id = randStr() + uuid(8)) : {};
+  DomNode[`${node.id}`] = {};
+
+  !allNodes.hasOwnProperty(Object.keys(DomNode)[0])
+    ? (allNodes[`${Object.keys(DomNode)[0]}`] = {})
+    : {};
+
+  allNodes[`${Object.keys(DomNode)[0]}`][`parent`] = node.parentNode;
+  allNodes[`${Object.keys(DomNode)[0]}`][`attributes`] = node.attributes;
+
+  if (node.hasChildNodes() == true) {
+    for (var i = 0; i < node.children.length; i++) {
+      recursivelyCheckForNodes(node.children[i], allNodes);
+    }
+  }
+}
+
+/**** HELPERS  *****/
+// A package by: lukeed
+// Github Repository link: https://github.com/lukeed/uid/blob/master/src/index.js
+var IDX = 36,
+  HEX = "";
+while (IDX--) HEX += IDX.toString(36);
+
+function uuid(len) {
+  var str = "",
+    num = len || 11;
+  while (num--) str += HEX[(Math.random() * 36) | 0];
+  return str;
+}
+
+function randStr() {
+  return Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, "")
+    .substr(0, 5);
+}
