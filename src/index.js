0395d5c1d96d27a808934c0d4b889d8c2efad9e2
mar-tina <martinakhaemba@gmail.com>
2020-03-24 22:04:10 +0300

    Minor update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import { App } from "./app.js";
 import { Shadow, initApp } from "../index.js";
 
+// Entry is not similar to other shadow elements
 let Entry = new Shadow("entry-point", {
   template: self => /*html*/ `
     <div> 
-      <div id="my-app"> 
         <my-app> </my-app> 
         <my-app> </my-app> 
-      </div>
     </div>
   `
 });
